7137a7b633dba785c775844f33b0da3c8f011be6
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-09-28 15:39:17 +0530

    Create findNLast.cpp
    findNLast(head, n): Return the Nth node from end of the list. If N is greater than size of the list, return the last node of list (return -1 if list is empty).

---
+++ findNLast.cpp
@@ -0,0 +1,27 @@
+int findNLast(Node* head, int n){
+   int length=1;
+    Node* first = head;
+    Node* second = head;
+
+    if(head == NULL)
+    return -1;
+
+    while(head->next!=NULL){
+    length++;
+    head = head->next;
+    }
+
+    if(n>length)
+    return head->data;
+
+    for(int i=0 ; i<n; i++)
+        second = second->next;
+
+        while(second!=NULL){
+            first = first->next;
+            second = second->next;
+        }
+        
+        return first->data;
+  
+}
